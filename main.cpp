8b45711d79fae98f98a4573fa12c3c1bcebbc601
Alex <81694529+Clepfeed@users.noreply.github.com>
2024-10-29 00:15:19 +0300

    add cmp with gasusses method

--- main.cpp
+++ main.cpp
@@ -99,22 +99,75 @@ void find_x(vector<vector<double>>& A, vector<double>& z)
 	}
 }
 
+void choiceMainElement(vector<vector<double>>& matrix, int i, vector<double>& matB)
+{
+	double MainEl = matrix[i][i];
+	int indOfMainEl = i;
+	for (int j = i + 1; j < matrix[i].size(); j++)
+	{
+		if (abs(MainEl) < abs(matrix[j][i]))
+		{
+			indOfMainEl = j;
+			MainEl = matrix[j][i];
+		}
+	}
+	if (i == indOfMainEl && matrix[i][i] == 0.0)
+	{
+		cerr << "Matrix A inconsistent";
+		exit(EXIT_FAILURE);
+	}
+	swap(matrix[i], matrix[indOfMainEl]);
+	swap(matB[i], matB[indOfMainEl]);
+}
+
+void gaussMethodWTC(vector<vector<double>>& matrix, vector<double>& solution, vector<double>& matB)
+{
+	for (int i = 0; i < solution.size(); i++)
+	{
+		choiceMainElement(matrix, i, matB);
+		for (int j = i + 1; j < solution.size(); j++) // j - строка 
+		{
+			double mulTo = matrix[j][i] / matrix[i][i];
+			matrix[j][i] = 0;
+			for (int k = i + 1; k < solution.size(); k++) // k - столбец (элемент строки)
+			{
+				matrix[j][k] -= (matrix[i][k] * mulTo);
+			}
+			matB[j] -= (matB[i] * mulTo);
+		}
+	}
+	for (int i = solution.size() - 1; i >= 0; i--)
+	{
+		for (int q = i + 1; q < solution.size(); q++)
+		{
+			matB[i] -= matrix[i][q] * solution[q];
+		}
+		solution[i] = matB[i] / matrix[i][i];
+	}
+}
+
 int main()
 {
 	srand(time(0));
 	/*fout << std::fixed << std::setprecision(22);*/ //Если надо установить больше знаков после запятой
 	int n = 1000;
 	vector<vector<double>> matA; // можем хранить только нижний треугольный вид, тк она симметрична 
+	vector<vector<double>> matAGa(n, vector<double> (n, 0)); // матрица для Гаусса
 	vector<double> solution(n, 0);
 	vector<double> impreciseSolution(n, 0);
+	vector<double> impreciseSolutionGa(n, 0); // Для Гаусса
 	vector<double> matB(n, 0);
+	vector<double> matBGa(n, 0); // для Гаусса
 	
 	for (int i = 0; i < n; i++) // заполняем матрицу А
 	{
 		vector <double> temp;
 		for (int q = 0; q < i; q++)
 		{
-			temp.push_back(rand() % 201 - 100.0);
+			double tempEl = rand() % 201 - 100.0;
+			temp.push_back(tempEl);
+			matAGa[i][q] = tempEl;
+			matAGa[q][i] = tempEl;
 		}
 		temp.push_back(0);
 		matA.push_back(temp);
@@ -130,13 +183,19 @@ int main()
 		{
 			sum += abs(matA[q][i]);
 		}
-		matA[i][i] = rand() % 120 + 12.0 + sum;
+		double diag = rand() % 120 + 12.0 + sum;
+		matA[i][i] = diag;
+		matAGa[i][i] = diag;
 	}
+	/*printVec(matA);
+	cout << "\n";
+	printVec(matAGa);*/
 	for (int i = 0; i < n; i++) // точные решения
 	{
 		solution[i] = 12.0 + i;
 	}
 	mulMat(matA, solution, matB); // перемножаем матрицы
+	mulMat(matA, solution, matBGa); // перемножаем матрицы
 	// получили матрицу b
 
 	fout << "Solution:\n";
@@ -172,5 +231,30 @@ int main()
 	fout << "Relative error: " << normSolImprecise / normSol << "\n";
 	
 
+	// Гаус
+
+	start = chrono::high_resolution_clock::now();
+
+	gaussMethodWTC(matAGa, impreciseSolutionGa, matBGa);
+
+	end = chrono::high_resolution_clock::now();
+	duration = end - start;
+
+	fout << "\n\n";
+	printVec(impreciseSolutionGa);
+
+	fout << "\nTime: " << duration.count() << " seconds\n\n";
+
+	normSol = 0;
+	for (int i = 0; i < solution.size(); i++)
+	{
+		normSol += abs(solution[i]);
+	}
+	normSolImprecise = 0;
+	for (int i = 0; i < solution.size(); i++)
+	{
+		normSolImprecise += abs(solution[i] - impreciseSolutionGa[i]);
+	}
+	fout << "Relative error: " << normSolImprecise / normSol << "\n";
 	return 0;
 }
